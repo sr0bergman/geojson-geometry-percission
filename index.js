ee8b36b58090dc98755e39c9a5c90dabd3058fb9
Shane Bergman <SBergman@manhard.com>
2018-01-15 14:52:01 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const meta = require('@turf/meta');
+const Turf = require('turf');
+const Fs = require('fs');
+//IN FILE
+const features = JSON.parse(Fs.readFileSync('features.json'));
+//CREATE NEW OUTFILE
+Fs.writeFile('output-percision.json', '', (err) => {
+    if (err) throw err;
+});
+
+
+meta.featureEach(features, function (currentFeature, featureIndex) {
+  	meta.coordEach(currentFeature, function(coords) {
+		
+		let lon = coords[0].toFixed(6)
+		let lat = coords[1].toFixed(6)
+		currentFeature.geometry.coordinates = [lon,lat]
+	});
+	
+	Fs.appendFile('output-percision.json', JSON.stringify(currentFeature) + ',' + '\n',function(){
+		console.log('updated...');
+	});
+
+});
+
